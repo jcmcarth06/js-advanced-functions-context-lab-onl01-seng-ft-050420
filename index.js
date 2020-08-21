45cb8257b24f5740f87076ccd8f3672dd006c427
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-21 01:33:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,29 @@ function createTimeOutEvent(datestamp) {
   })
   return this;
 };
+
+function hoursWorkedOnDate(date){
+  let timeIn = this.timeInEvents.find( e => e.date === date);
+  let timeOut = this.timeOutEvents.find( e => e.date === date);
+  let hours = (timeOut.hour = timeIn.hour)/100;
+  return hours;
+};
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+};
+
+let allWagesFor = function () {
+    let eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
